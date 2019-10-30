6f81b49dcf6dd9bea8469b167ee4681c5476de5a
irinael <52750054+irinael@users.noreply.github.com>
2019-10-30 11:25:06 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -7,13 +7,10 @@ public class Greeter {
 
   /**constructeur.**/
   public Greeter() {
-  
-
   }
 /**methode bonjour.
 @return message coucou
 @param someone : nom de la personne à saluer
-
 **/
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
